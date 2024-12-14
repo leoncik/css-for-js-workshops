f2b580ff02a96e3ebd3e100e73c40c40f4832a25
Léonard Wojcik <leonardw@tutanota.com>
2024-12-14 10:44:33 +0100

    feat(exercise 4): add background decorator

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import BackgroundDecorator from './components/BackgroundDecorator';
 
 function App() {
   return (
     <>
+      <BackgroundDecorator />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/BackgroundDecorator/BackgroundDecorator.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './BackgroundDecorator.module.css';
+
+const BackgroundDecorator = () => {
+  return <div className={styles.background}></div>;
+};
+
+export default BackgroundDecorator;
